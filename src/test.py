7783d65b06c183b2c785d512784a0e4d578fd3b1
sharonwu <astrumstella048@gmail.com>
2016-08-16 16:59:49 +0800

    revise test.py

--- src/test.py
+++ src/test.py
@@ -12,20 +12,58 @@ import fifa
 def test():
     path = os.path.dirname(os.path.abspath(__file__))
 
-    def test_schema(result):
+    def test_schema(event_dict_list):
+        event_list = ["goal", "goal-own", "yellow-card", "red-card", "yellow-card-second",
+                      "substitution-in-out", "substitution-out", "substitution-in",
+                      "first-start", "first-end", "second-start", "second-end",
+                      "third-start", "third-end", "penalty", "penalty-wrong",
+                      "match-report", "hidden"]
         schema_correct = True
-        if isinstance(result, list):
-            for event in result:
-                if not isinstance(event, dict) or not len(event) == 4:
+        if isinstance(event_dict_list, list):
+            for event_dict in event_dict_list:
+                if isinstance(event_dict, dict):
+                    video = event_dict["video"]
+                    if isinstance(video, str):
+                        if not video.endswith(".mp4"):
+                            print ("Video name is not correct")
+                            schema_correct = False
+                            break
+                    else:
+                        print ("The type of video is wrong")
+                        schema_correct = False
+                        break
+
+                    event = event_dict["event"]
+                    if isinstance(event, str):
+                        if not event in event_list:
+                            print ("Unknown event")
+                            schema_correct = False
+                            break
+                    else:
+                        print ("The type of event is wrong")
+                        schema_correct = False
+                        break
+
+                    message = event_dict["message"]
+                    if not isinstance(message, str):
+                        schema_correct = False
+                        break
+
+                    time = event_dict["time"]
+                    if not isinstance(time, str):
+                        schema_correct = False
+                        break
+
+                else:
+                    print ("An element in the parser result should be a dict")
                     schema_correct = False
                     break
         else:
+            print ("Parser result is not a list.")
             schema_correct = False
 
-        if schema_correct is True:
-            return True
-        else:
-            return False
+        return schema_correct
+
 
     def test_content(filepath_result, filepath_target):
         with open(filepath_result) as f1, open(filepath_target) as f2:
@@ -35,9 +73,10 @@ def test():
                     content_correct = False
                     break
             if content_correct is True:
-                print ("Parse Video Succeed!")
+                print ("Parsing match Succeeds!")
             else:
-                print ("Parse Video Fail...")
+                print ("Parsing match Fails.")
+
 
     folder = u"2014世界盃"
     video = "128848_3500K_384K_1920x1080_00-00-01_14511_SH14052001-068.mp4"
@@ -56,7 +95,6 @@ def test():
         targetfile.close()
 
         # Test result content
-        print ("Testing result content.")
         filepath_target = path + '/target/target_1'
         test_content(filepath_result, filepath_target)
     else:
